a378d5019cd4232e4b35d11e031f68eaf1cf66c2
Aditya Pratap <adityapratap7344@gmail.com>
2020-11-18 03:42:53 +0530

    created a basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,24 @@
+import express, { Express, Request, Response } from 'express';
+import http from 'http';
 import chalk from "chalk";
+import { config as dotenvConfig } from 'dotenv';
+import cors from 'cors';
 
-console.log(chalk.blue("Hello World"));
+dotenvConfig();
+
+const app: Express = express();
+
+app.use(cors());
+
+app.get('/', (_req: Request, res: Response) => {
+    res.send('Welcome to Familia Server!');
+});
+
+const mainServer = http.createServer(app);
+
+const PORT = process.env.PORT;
+
+mainServer.listen(PORT, () => {
+    const localURL = `http://localhost:${PORT}`;
+    console.log(`\nServer is ready at ${chalk.blueBright(localURL)}`);
+});
