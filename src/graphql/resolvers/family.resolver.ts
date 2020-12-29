1c4a5324be58a717380529acbf70e99fe8405432
Aditya Pratap <adityapratap7344@gmail.com>
2020-12-29 21:29:04 +0530

    refactored family.resolver to use user validator

--- src/graphql/resolvers/family.resolver.ts
+++ src/graphql/resolvers/family.resolver.ts
@@ -1,15 +1,11 @@
 import { IFieldResolver, IResolvers } from 'graphql-tools';
-import { UserInputError } from 'apollo-server-express';
-// import chalk from 'chalk';
-// import util from 'util';
 import { DateTimeResolver } from 'graphql-scalars';
 import { authCheck } from '../helpers/auth';
 import { ContextAttributes } from '../helpers/context';
 import { FamilyDoc } from '../../models/family.model';
 import FamilyService from '../../services/family.service';
-import UserService from '../../services/user.service';
-import { UserDoc } from '../../models/user.model';
-import { FamilyErrors, getGraphqlError, UserErrors } from '../../errors';
+import { FamilyErrors, getGraphqlError } from '../../errors';
+import { UserValidators } from '../validators';
 
 interface newFamilyArgs {
     input: {
@@ -28,29 +24,20 @@ const allFamilies: IFieldResolver<any, ContextAttributes, any, Promise<FamilyDoc
 }
 
 const createFamily: IFieldResolver<any, ContextAttributes, newFamilyArgs, Promise<FamilyDoc>> = async (source, args, context) => {
-    const userRecord = await authCheck(context.req);
+    const userAuthRecord = await authCheck(context.req);
 
     if (!args.input.name.trim()) {
         throw getGraphqlError(FamilyErrors.userInput.nameRequired);
     }
 
-    let createdByUser: UserDoc | null;
-    try {
-        createdByUser = await UserService.getOneUserByAuthId(userRecord.uid);
-
-        if (!createdByUser) {
-            throw UserErrors.general.userNotFound;
-        }
-    } catch (error) {
-        throw getGraphqlError(error);
-    }
+    const requestingUser = await UserValidators.checkIfUserExists(userAuthRecord);
 
     try {
         const { name, description } = args.input;
         const createdFamily = await FamilyService.createFamilyAndAddMembership({
             name,
             description,
-            creator: createdByUser._id as string,
+            creator: requestingUser._id as string,
         });
 
         return createdFamily;
