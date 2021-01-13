7d7491611577e0f03219fcba58a752efc5700300
Aditya Pratap <adityapratap7344@gmail.com>
2021-01-14 03:16:07 +0530

    created message.typeDef

---
+++ src/graphql/typeDefs/message.typeDef.ts
@@ -0,0 +1,41 @@
+import { gql } from 'apollo-server-express';
+
+const messageType = gql`
+    type Message {
+        _id: ID!
+        text: String!
+        from: String!
+        to: String!
+        family: String!
+        createdAt: DateTime!
+        updatedAt: DateTime!
+    }
+
+    input CreateMessageInput {
+        text: String!
+        from: String!
+        to: String!
+        family: String!
+    }
+
+    input AllChatMessagesInput {
+        familyId: String!
+        from: String!
+        to: String!
+    }
+
+    type Query {
+        totalMessages: Int!
+        allChatMessages(input: AllChatMessagesInput!): [Message!]!
+    }
+
+    type Mutation {
+        createMessage(input: CreateMessageInput!): Message!
+    }
+
+    type Subscription {
+        onMessageAdded: Message
+    }
+`;
+
+export default messageType;
